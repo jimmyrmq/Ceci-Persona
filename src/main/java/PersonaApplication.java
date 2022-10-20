766f52d69fe0c06d111bae30b2c48a4609fbb807
Jimmy Morales <jimmyrmq@gmail.com>
2022-10-20 15:10:55 -0300

    Ceci-Persona

--- src/main/java/PersonaApplication.java
+++ src/main/java/PersonaApplication.java
@@ -1,10 +1,7 @@
-package com.disoft.ceci.persona;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
 //@ComponentScan(value = "com.disoft.ceci.persona")
