b925520d923efd8c940d95fb9bd329e40a0f5c80
Jimmy Morales <jimmyrmq@gmail.com>
2022-10-26 02:29:16 -0300

    Ceci-Persona

--- src/main/java/com/disoft/ceci/persona/PersonaApplication.java
+++ src/main/java/com/disoft/ceci/persona/PersonaApplication.java
@@ -28,7 +28,7 @@ public class PersonaApplication extends SpringBootServletInitializer {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("GET").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500").allowedMethods("GET").allowedHeaders("*");
 			}
 		};
 	}
